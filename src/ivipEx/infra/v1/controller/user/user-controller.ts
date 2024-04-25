0fe9922855143d98e0e2c6d318224f59fbed4075
iamrosada <luisrosada2018@gmail.com>
2024-04-25 20:14:48 +0300

    feat: updating the user controller

--- src/ivipEx/infra/v1/controller/user/user-controller.ts
+++ src/ivipEx/infra/v1/controller/user/user-controller.ts
@@ -1,16 +1,31 @@
 import { Request, Response } from 'express';
 
-import { UserRepository } from '../../../../application/repositories/user-repository';
+import { UserUseCase } from '../../../../application/usecases/user/create-user';
 
 export class UserController {
-  constructor(private UserRepository: UserRepository) {}
+  constructor(private userUseCase: UserUseCase) {}
 
   async registerUser(req: Request, res: Response): Promise<void> {
     try {
-      const { email } = req.body;
+      const { password, email } = req.body;
 
-      await this.UserRepository.registerUser(email);
+      await this.userUseCase.registerUser(email, password);
       res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async findById(req: Request, res: Response): Promise<void> {
+    try {
+      const { userId } = req.params;
+
+      const user = await this.userUseCase.getDetailUser(userId);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
---
+++ src/ivipEx/infra/v1/http/user.ts
@@ -0,0 +1,20 @@
+import { UserController } from '../controller/user/user-controller';
+import { PrismaUsersRepository } from '../repositories/prisma-customer-repository';
+import { UserUseCase } from '../../../application/usecases/user/create-user';
+
+/**
+ * Iniciar Repository
+ */
+const UserRepo = new PrismaUsersRepository();
+
+/**
+ * Iniciamos casos de uso
+ */
+
+const userUseCase = new UserUseCase(UserRepo);
+
+/**
+ * Iniciar User Controller
+ */
+
+export const UserCtrl = new UserController(userUseCase);
