6d32b63f495f0dd8172e283bc54000b02e348b14
iamrosada <luisrosada2018@gmail.com>
2024-04-26 18:40:02 +0300

    feat: updated the userRepository

--- src/ivipEx/application/repositories/user-repository.ts
+++ src/ivipEx/application/repositories/user-repository.ts
@@ -2,6 +2,6 @@ import { UserEntity } from '../../domain/user/entity/user';
 
 export interface UserRepository {
   findById(id: string): Promise<UserEntity | null>;
-  findByUserName(name: string): Promise<UserEntity | null>;
+  findByEmail(email: string): Promise<UserEntity | null>;
   registerUser(user: UserEntity): Promise<void>;
 }
