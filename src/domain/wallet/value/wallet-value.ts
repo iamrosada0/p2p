e5b5c73a9810a5c4f71ca2e795a437a4346b7062
iamrosada <luisrosada2018@gmail.com>
2024-04-30 17:22:08 +0300

    feat: created the constructor  to generate the user wallet

---
+++ src/domain/wallet/value/wallet-value.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from 'crypto';
+import Wallet from 'ethereumjs-wallet';
+
+import { WalletEntity } from '../entity/wallet';
+
+export class WalletValue implements WalletEntity {
+  public uuid: string;
+  public publicKey: string;
+  public privateKey: string;
+  public addressUserWallet: string;
+  public userId: string;
+  public createdAt: Date | undefined;
+  public updatedAt: Date | undefined;
+
+  constructor({ userId }: Omit<WalletValue, 'uuid'>) {
+    this.uuid = randomUUID();
+    this.privateKey = Wallet.generate().getPrivateKeyString();
+    this.userId = userId;
+    this.publicKey = Wallet.fromPrivateKey(Buffer.from(this.privateKey, 'hex')).getPublicKeyString();
+
+    // Derivar o endereço da carteira do privado
+    this.addressUserWallet = Wallet.fromPrivateKey(Buffer.from(this.privateKey, 'hex')).getChecksumAddressString();
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
