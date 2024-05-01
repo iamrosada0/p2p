f11fce9e3eefb6c857100d63521c16d57bc7046c
iamrosada <luisrosada2018@gmail.com>
2024-05-01 17:00:47 +0300

    feat: added more fields on transaction method on transaction controller

--- src/infra/controller/transaction/transaction-controller.ts
+++ src/infra/controller/transaction/transaction-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { TransactionUseCase } from '../../../application/usecases/transaction/create-transacttion';
+import { TransactionUseCase } from '../../../application/usecases/transaction/create-transaction';
 
 export class TransactionController {
   constructor(private transactionUseCase: TransactionUseCase) {
@@ -8,9 +8,9 @@ export class TransactionController {
 
   async transaction(req: Request, res: Response): Promise<void> {
     try {
-      const { userId } = req.body;
+      const { userId, toAddress, amountInEther } = req.body;
 
-      await this.transactionUseCase.transaction(userId);
+      await this.transactionUseCase.transaction(userId, toAddress, amountInEther);
       res.status(201).json({ message: 'User transaction processed successfully' });
     } catch (error: unknown) {
       if (error instanceof Error) {
