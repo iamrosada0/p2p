dd3ade748acea0c78ece8c04fee86170f083db52
iamrosada <luisrosada2018@gmail.com>
2024-05-01 16:55:52 +0300

    feat: added more entity on return

--- src/application/repositories/transactions-repository.ts
+++ src/application/repositories/transactions-repository.ts
@@ -1,5 +1,6 @@
+import { TransactionReceipt } from 'web3';
 import { TransactionEntity } from '../../domain/transaction/entity/transactions';
 
 export interface TransactionRepository {
-  transaction(transaction: TransactionEntity): Promise<TransactionEntity>;
+  transaction(transaction: TransactionEntity | TransactionReceipt): Promise<TransactionEntity | TransactionReceipt>;
 }
