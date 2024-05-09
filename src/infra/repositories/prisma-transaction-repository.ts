54501af2cff87b895e67683707cb97a26d45c2c0
iamrosada <luisrosada2018@gmail.com>
2024-05-10 01:07:38 +0300

    feat: added check

--- src/infra/repositories/prisma-transaction-repository.ts
+++ src/infra/repositories/prisma-transaction-repository.ts
@@ -4,22 +4,44 @@ import { TransactionEntity } from '../../domain/transaction/entity/transactions'
 import { prisma } from '../database/prisma/prisma';
 
 export class PrismaTransactionRepository implements TransactionRepository {
-  async transaction(transaction: TransactionEntity): Promise<TransactionEntity> {
-    const transactionCreated = await prisma.transaction.create({
-      data: {
-        uuid: transaction.uuid,
-        from: transaction.addressUserWallet,
-        useId: transaction.userId,
-        value: transaction.value as string,
-        to: transaction.recipientAddress,
-        gasprice: String(transaction.gasPrice),
-        gas: String(transaction.gasPrice),
-        nonce: String(transaction.nonce),
-        updatedAt: String(transaction.updatedAt),
-        createdAt: String(transaction.createdAt),
+  async checkForNewTransactions(userId: string): Promise<TransactionEntity[] | any> {
+    const newTransactions = await prisma.transaction.findMany({
+      where: {
+        userId: userId,
       },
     });
 
-    return transactionCreated as any;
+    return newTransactions;
+  }
+  async transaction(transaction: TransactionEntity): Promise<TransactionEntity> {
+    console.log(transaction, 'transaction-->DEUS-1');
+    const transactionCreated = await prisma.transaction.create({
+      data: {
+        uuid: transaction.uuid, // Ensure uuid is provided
+        from: transaction.addressUserWallet,
+        userId: transaction.userId,
+        value: transaction.value, // No need to cast value to string
+        to: transaction.recipientAddress,
+        gasprice: transaction?.gasPrice?.toString(), // Ensure gasPrice is provided and cast to string
+        gas: transaction?.gasLimit?.toString(), // Ensure gasLimit is provided and cast to string
+        nonce: transaction?.nonce?.toString(), // Ensure nonce is provided and cast to string
+        updatedAt: transaction?.updatedAt?.toString() as unknown as string, // Ensure updatedAt is provided
+        createdAt: transaction?.createdAt?.toString() as unknown as string, // Ensure createdAt is provided
+      },
+    });
+    const transactionData: TransactionEntity = {
+      uuid: transactionCreated.uuid,
+      recipientAddress: transactionCreated.from,
+      gasLimit: transactionCreated.gasprice as unknown as number,
+      gasPrice: transactionCreated.gasprice as unknown as number,
+      nonce: transactionCreated.nonce as unknown as number,
+      value: transactionCreated.value,
+      privateKey: '',
+      publicKey: '',
+      addressUserWallet: '',
+      userId: transactionCreated.userId,
+    };
+
+    return transactionData;
   }
 }
