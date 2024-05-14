6d605086f2d0941e9ed4b6e6a2b1b47c01068c4e
iamrosada <luisrosada2018@gmail.com>
2024-05-14 16:41:52 +0300

    feat: added barrel on domain

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,3 +1,7 @@
+export * from './custody_of_funds/entity/custody';
+export * from './custody_of_funds/value/custody-value';
+export * from './transaction/entity/transactions';
+export * from './transaction/value/transaction-value';
 export * from './user/entity/user';
 export * from './user/value/user-value';
 export * from './wallet/entity/wallet';
