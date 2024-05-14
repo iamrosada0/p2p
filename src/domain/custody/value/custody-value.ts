8209c8af654184c02e44efe76dc589e9e747c3a9
iamrosada <luisrosada2018@gmail.com>
2024-05-14 21:51:32 +0300

    feat: created custody value

--- src/domain/custody/value/custody-value.ts
+++ src/domain/custody/value/custody-value.ts
@@ -7,14 +7,16 @@ export class UserFundsInCustodyValue implements UserFundsInCustodyEntity {
   public userId: string;
   public amount: number;
   public walletId: string;
+  public to?: string;
   public cryptoType: string;
   public createdAt: Date;
   public updatedAt: Date;
 
-  constructor(userId: string, funds: number, typeCrypto: string, walletId: string) {
+  constructor(userId: string, funds: number, typeCrypto: string, walletId: string, to?: string) {
     this.userId = userId;
     this.uuid = randomBytes(16).toString('hex');
     this.amount = funds;
+    this.to = to;
     this.cryptoType = typeCrypto;
     this.walletId = walletId;
     this.createdAt = new Date();
