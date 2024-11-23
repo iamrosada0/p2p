0910f53ebe0c464a3be537491b2cff49ba1b8773
iamrosada0 <luisrosada@outlook.com>
2024-11-23 16:55:54 +0100

    feat: injection

--- src/infra/core/injection-dependency/injection.ts
+++ src/infra/core/injection-dependency/injection.ts
@@ -10,27 +10,31 @@ import { PrismaCustodyRepository } from '../../repositories/prisma-custody-repos
 const userRepo = new PrismaUsersRepository();
 const walletRepo = new PrismaWalletRepository();
 const transactionRepo = new PrismaTransactionRepository();
-const custodyRepo = new PrismaCustodyRepository();
+const custodyRepo = new PrismaCustodyRepository(
+  process.env.APP_INFURA_ID_PROJECT!,
+  '3SED3AGJMV6YN2GHQFTJN5JP28TGKD1QBZ',
+);
 /**
  * Iniciamos casos de uso
  */
-const custodyUseCase = new CustodyUseCase(
-  custodyRepo,
-  walletRepo,
-  process.env.APP_INFURA_ID_PROJECT!,
-  '0x1F0401676682EE49838F298F24EE4AC4E1349F2e',
-);
+
+new CustodyUseCase(process.env.APP_INFURA_ID_PROJECT!, '3SED3AGJMV6YN2GHQFTJN5JP28TGKD1QBZ');
+
+// custodyRepo.fetchAndCheckWalletValues();
+const interval = setInterval(() => {
+  custodyRepo.fetchAndCheckWalletValues();
+}, 6000); // 6000 milissegundos = 6 segundos
+
+// Cancelar o setInterval() após um determinado tempo (opcional)
+// Isso evitará que a função continue sendo chamada indefinidamente
+// Aqui, cancelamos após 1 hora (3600000 milissegundos = 1 hora)
+setTimeout(() => {
+  clearInterval(interval);
+}, 3600000); //
 
 const userUseCase = new UserUseCase(userRepo);
 const walletUseCase = new WalletUseCase(walletRepo);
-const transactionUseCase = new TransactionUseCase(
-  transactionRepo,
-  walletRepo,
-  custodyRepo,
-  custodyUseCase,
-
-  process.env.APP_INFURA_ID_PROJECT!,
-);
+const transactionUseCase = new TransactionUseCase(transactionRepo, walletRepo, process.env.APP_INFURA_ID_PROJECT!);
 
 /**
  * Iniciar  Controller
