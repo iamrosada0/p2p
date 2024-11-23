0922c4dbbd993bbc88a437827ad911607b947d09
iamrosada0 <luisrosada@outlook.com>
2024-11-23 16:54:37 +0100

    feat: find all metod

--- src/infra/repositories/prisma-wallet-repository.ts
+++ src/infra/repositories/prisma-wallet-repository.ts
@@ -3,6 +3,9 @@ import { WalletEntity } from '../../domain';
 import { prisma } from '../database/prisma/prisma';
 
 export class PrismaWalletRepository implements WalletRepository {
+  findAllUsers(): Promise<[] | WalletEntity[]> {
+    throw new Error('Method not implemented.');
+  }
   async findDetailWalletByUserId(userId: string): Promise<WalletEntity | null> {
     const foundUser = await prisma.wallet.findFirst({
       where: { userId: userId },
